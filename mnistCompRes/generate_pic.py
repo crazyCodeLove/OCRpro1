431042c9893c7cf9ef5230f147b9e6ce99353b07
allen <zhaopengcheng11@163.com>
2018-04-28 15:46:51 +0800

    add skeleton filter

--- mnistCompRes/generate_pic.py
+++ mnistCompRes/generate_pic.py
@@ -6,6 +6,8 @@ from skimage.morphology import disk
 from skimage.filters import rank
 from matplotlib import pyplot as plt
 from skimage import filters
+from skimage import feature
+from skimage import morphology
 
 from PIL import Image
 
@@ -56,8 +58,6 @@ def gauss_filter():
 
         medium_img = rank.median(resize_img,selem=disk(3))
 
-
-
         plt.figure()
         plt.subplot(2,2,1)
         plt.title('ori 28x28')
@@ -74,6 +74,71 @@ def gauss_filter():
         # plt.subplot(2, 2, 4)
         # plt.imshow(medium_img)
         plt.show()
+
+def canny_filter():
+    gen = get_single_img_label()
+    for img, label in gen:
+        img = resize_pic(img, [96, 96])
+        gauss_img = filters.gaussian(img, sigma=0.9)
+        medium_img = rank.median(gauss_img,selem=disk(3))
+
+
+        canny_img = feature.canny(gauss_img, sigma=3)*1
+        canny_img2 = feature.canny(medium_img,sigma=3)*1
+
+        canny_img = np.asanyarray(canny_img,dtype=np.float32)
+        canny_img2 = np.asanyarray(canny_img2,dtype=np.float32)
+
+        plt.figure()
+        plt.subplot(2, 2, 1)
+        plt.title(np.argmax(label))
+        plt.imshow(canny_img2)
+
+        plt.subplot(2, 2, 2)
+        plt.imshow(gauss_img)
+        plt.title('gauss_img')
+
+        plt.subplot(2, 2, 3)
+        plt.imshow(medium_img)
+        plt.title('medium_img')
+
+        plt.subplot(2, 2, 4)
+        plt.imshow(canny_img)
+        plt.title('canny_img')
+        plt.show()
+
+def skeleton_retrive():
+    gen = get_single_img_label()
+    for img, label in gen:
+        img = resize_pic(img, [96, 96])
+        gauss_img = filters.gaussian(img, sigma=0.9)
+        medium_img = rank.median(gauss_img, selem=disk(3))
+
+        val = filters.threshold_otsu(medium_img)
+        twovalue = (medium_img > val)*1
+        twovalue = np.asanyarray(twovalue,dtype=np.uint8)
+
+        skeleton_img = morphology.skeletonize(twovalue)
+
+
+        plt.figure()
+        plt.subplot(1, 2, 1)
+        plt.imshow(gauss_img)
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(skeleton_img)
+        plt.show()
+
+def same_number():
+    gen = get_single_img_label()
+    for img, label in gen:
+        if(np.argmax(label) == 6):
+            plt.figure()
+            plt.imshow(img)
+            plt.show()
+
+
+
 
 def strage_1():
     gen = get_single_img_label()
@@ -105,8 +170,11 @@ def strage_1():
 
 def main():
     # gen_salt_filter()
-    gauss_filter()
+    # gauss_filter()
     # strage_1()
+    # canny_filter()
+    # skeleton_retrive()
+    same_number()
 
 if __name__ == "__main__":
     main()
