f303677286e74a3501e60fa185db98b5a4adf027
allen <zhaopengcheng11@163.com>
2018-05-14 21:24:16 +0800

    add deepc gabor model

---
+++ deepCNetv1/deepcmain.py
@@ -0,0 +1,172 @@
+from collections import namedtuple
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import pickle
+
+from mnistCompRes.mnisttool import imgUtil
+from mnistCompRes.mnisttool import ModelUtilv3s1
+from deepCNet.modelUtil import deepcnetModel
+
+HParams = namedtuple('HParams',
+                     'batch_nums, num_classes, deep_net_fkn,'
+                     'img_depth, img_width, des_img_size')
+
+
+logger = ModelUtilv3s1.MyLog('/home/allen/work/data/resultlog/deepcNet/deepcnet9.txt')
+logDir = '/home/allen/work/data/resultlog/deepcNet/summary/deepcs96f50BN3'
+
+mnist = input_data.read_data_sets("../MNIST_DATA/", one_hot=True)
+save_file = "/home/allen/work/variableSave/deepcnet/deepcnet.ckpt"
+peizhi_filename = "/home/allen/work/chengxu/OCR/OCRpro1/deepCNet/peizhi.xml"
+
+peizhi_dict = {'lrn_rate':1e-3,
+               'is_restore':False,
+               'train_step':0,
+               'max_test_acc':0}
+
+train_writer = tf.summary.FileWriter(logDir)
+
+def startTrain(hps, mode):
+    tf.reset_default_graph()
+    with open(peizhi_filename, mode='rb') as rfobj:
+        peizhi = pickle.load(rfobj)
+
+    xp = tf.placeholder(tf.float32, [hps.batch_nums, hps.des_img_size, hps.des_img_size, hps.img_depth])
+    yp = tf.placeholder(tf.float32, [None, 10])
+    model = deepcnetModel.DeepCModel(hps, xp, yp, mode, peizhi['train_step'])
+    model.create_graph()
+
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+        saver = tf.train.Saver()
+
+        if not peizhi['is_restore']:
+            sess.run(model.init)
+        else:
+            saver.restore(sess, save_file)
+
+        base_step = sess.run(model.step)
+        end_step = int(base_step + 10000 + 1)
+        for itstep in range(base_step,end_step):
+            if (itstep%10) <= 8:
+                images,labels = mnist.train.next_batch(hps.batch_nums)
+            else:
+                images,labels = mnist.validation.next_batch(hps.batch_nums)
+
+            images = batch_imgs_process(images,hps)
+
+            feed_dict = {
+                xp:images,
+                yp:labels,
+                model.learning_rate: peizhi['lrn_rate'],
+                model.is_training_ph: True}
+            (inlabels,outprediction,cost,_ ) = sess.run(
+                [model.labes, model.predictions, model.loss, model.train_op],
+                feed_dict=feed_dict)
+
+            if itstep % 100 == 0:
+                (inlabels, outprediction, cost, _, summary) = sess.run(
+                    [model.labes, model.predictions, model.loss, model.train_op, model.merged],
+                    feed_dict=feed_dict)
+                trainacc = ModelUtilv3s1.get_accurate(outprediction, inlabels)
+                msg = "trainstep:%5d  loss:%e  train acc:%.5f"%(itstep, cost, trainacc)
+                logger.log_message(msg)
+                train_writer.add_summary(summary, itstep)
+
+                if itstep % 500 == 0:
+                    logger.showAndLogMsg(msg)
+
+
+        print("before save")
+
+        saver.save(sess=sess,save_path=save_file)
+        print("after save")
+        ModelUtilv3s1.update_peizhi(peizhi_filename,'is_restore',True)
+        end_step = sess.run(model.step)
+        ModelUtilv3s1.update_peizhi(peizhi_filename,'train_step',end_step)
+
+def startTest(hps, mode):
+    tf.reset_default_graph()
+    with open(peizhi_filename, mode='rb') as rfobj:
+        peizhi = pickle.load(rfobj)
+    lrn_rate = peizhi['lrn_rate']
+
+    xp = tf.placeholder(tf.float32, [hps.batch_nums, hps.des_img_size, hps.des_img_size, hps.img_depth])
+    yp = tf.placeholder(tf.float32, [None, 10])
+
+    model = deepcnetModel.DeepCModel(hps, xp, yp, mode, peizhi['train_step'])
+    model.create_graph()
+
+    # 训练一遍需要的步数
+    epochTrainNums = int(10000 // hps.batch_nums)
+    allrightnums = 0
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+        saver = tf.train.Saver()
+
+        saver.restore(sess=sess, save_path=save_file)
+
+        for itstep in range(0, epochTrainNums):
+            images, labels = mnist.test.next_batch(hps.batch_nums)
+            images = batch_imgs_process(images,hps)
+
+            feed_dict = {
+                xp: images,
+                yp: labels,
+                model.is_training_ph: False}
+            (inlabels, outprediction) = sess.run(
+                [model.labes, model.predictions],
+                feed_dict=feed_dict)
+            itrightnums = ModelUtilv3s1.get_test_right_num(outprediction, inlabels)
+            allrightnums += itrightnums
+
+
+        test_acc = float(allrightnums)/10000
+        lrn_rate = ModelUtilv3s1.down_learning_rate(test_acc,lrn_rate)
+        ModelUtilv3s1.update_peizhi(peizhi_filename,'lrn_rate',lrn_rate)
+
+        if test_acc > peizhi['max_test_acc']:
+            ModelUtilv3s1.update_peizhi(peizhi_filename,'max_test_acc',test_acc)
+
+
+        msg = "test acc:%.5f         now learning rate:%f"%(test_acc,lrn_rate)
+
+        logger.showAndLogMsg(msg)
+
+def batch_imgs_process(batch_imgs, hps):
+    batch_imgs = imgUtil.batch_imgs_reshape(batch_imgs, hps)
+    batch_imgs = imgUtil.batch_imgs_resize(batch_imgs, hps.des_img_size, hps)
+    batch_imgs = imgUtil.batch_imgs_pre_process(batch_imgs)
+    return batch_imgs
+
+
+def main():
+    hps = HParams(batch_nums=50,
+                  num_classes=10,
+                  deep_net_fkn=100,
+                  img_depth=1,
+                  img_width=28,
+                  des_img_size=96
+                  )
+    ModelUtilv3s1.init_peizhi(
+        peizhifilename=peizhi_filename, peizhidict=peizhi_dict)
+
+    while True:
+        print("start training")
+        mode = 'train'
+        startTrain(hps,mode=mode)
+        print("training end")
+
+        print("start test")
+        mode = 'test'
+
+        startTest(hps,mode=mode)
+
+        print("test end")
+
+        with open(peizhi_filename, mode='rb') as rfobj:
+            peizhi = pickle.load(rfobj)
+        if peizhi['max_test_acc'] >= 0.999:
+            print("already over best test acc, now test acc is ", peizhi['max_test_acc'])
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ deepCNetv1/modelUtil/deepcnetModel.py
@@ -0,0 +1,184 @@
+import tensorflow as tf
+
+def conv2fc(inputs):
+    conv_out_shape = inputs.get_shape().as_list()
+    fcl_in_features = conv_out_shape[1] * conv_out_shape[2] * conv_out_shape[3]
+    fcl_inputs = tf.reshape(inputs, [-1, fcl_in_features])
+    return fcl_inputs,fcl_in_features
+
+class DeepCModel(object):
+
+    def __init__(self,hps,images,labels, mode, init_step):
+        self.hps = hps
+        self._images = images
+        self.labes = labels
+        self.mode = mode
+        self.activateFunction=tf.nn.relu
+        self.init_step = init_step
+
+    def create_resnet(self, inputx, in_training, activateFunc=tf.nn.relu):
+        # inputs:96
+
+        cl1_kernal = 3
+        cl1_in_depth = self.hps.img_depth
+        cl1_out_depth = self.hps.deep_net_fkn*1
+        cl1_layer_name = "layer1"
+        with tf.variable_scope(cl1_layer_name):
+            # conv1:94
+            conv1_weight = tf.get_variable('weight',
+                                           [cl1_kernal, cl1_kernal, cl1_in_depth, cl1_out_depth],
+                                           initializer=tf.truncated_normal_initializer(stddev=0.1))
+            # conv1_biases = tf.get_variable('bias', [cl1_out_depth])
+            conv1 = tf.nn.conv2d(inputx, conv1_weight, strides=[1, 1, 1, 1], padding='VALID')
+            # layer1_conv = tf.nn.bias_add(conv1, conv1_biases)
+            layer1_conv = conv1
+            layer1_conv = tf.layers.batch_normalization(layer1_conv, axis=0, training=in_training)
+            layer1_conv = activateFunc(layer1_conv)
+
+        cl2_layer_name = 'layer2'
+        # layer2_pool:47
+        with tf.variable_scope(cl2_layer_name):
+            layer2_pool = tf.nn.max_pool(layer1_conv, [1, 2, 2, 1], strides=[1, 2, 2, 1],
+                                         padding='SAME')
+
+        cl3_kernal = 2
+        cl3_in_depth = cl1_out_depth
+        cl3_out_depth = self.hps.deep_net_fkn * 2
+        cl3_layer_name = "layer3"
+        # layer3_conv:46
+        with tf.variable_scope(cl3_layer_name):
+            conv3_weight = tf.get_variable('weight',
+                                           [cl3_kernal, cl3_kernal, cl3_in_depth, cl3_out_depth],
+                                           initializer=tf.truncated_normal_initializer(stddev=0.1))
+            # conv3_biases = tf.get_variable('bias', [cl3_out_depth])
+            conv3 = tf.nn.conv2d(layer2_pool, conv3_weight, strides=[1, 1, 1, 1], padding='VALID')
+            # layer3_conv = tf.nn.bias_add(conv3, conv3_biases)
+            layer3_conv = conv3
+            layer3_conv = tf.layers.batch_normalization(layer3_conv, axis=0, training=in_training)
+            layer3_conv = activateFunc(layer3_conv)
+
+        cl4_layer_name = 'layer4'
+        # layer4_pool:23
+        with tf.variable_scope(cl4_layer_name):
+            layer4_pool = tf.nn.max_pool(layer3_conv, [1, 2, 2, 1], strides=[1, 2, 2, 1],
+                                         padding='SAME')
+
+        cl5_kernal = 2
+        cl5_in_depth = cl3_out_depth
+        cl5_out_depth = self.hps.deep_net_fkn * 3
+        cl5_layer_name = "layer5"
+        # layer5_conv:22
+        with tf.variable_scope(cl5_layer_name):
+            conv5_weight = tf.get_variable('weight',
+                                           [cl5_kernal, cl5_kernal, cl5_in_depth, cl5_out_depth],
+                                           initializer=tf.truncated_normal_initializer(stddev=0.1))
+            # conv5_biases = tf.get_variable('bias', [cl5_out_depth])
+            conv5 = tf.nn.conv2d(layer4_pool, conv5_weight, strides=[1, 1, 1, 1], padding='VALID')
+            # layer5_conv = tf.nn.bias_add(conv5, conv5_biases)
+            layer5_conv = conv5
+            layer5_conv = tf.layers.batch_normalization(layer5_conv, axis=0, training=in_training)
+            layer5_conv = activateFunc(layer5_conv)
+
+        cl6_layer_name = 'layer6'
+        # layer6_pool:11
+        with tf.variable_scope(cl6_layer_name):
+            layer6_pool = tf.nn.max_pool(layer5_conv, [1, 2, 2, 1], strides=[1, 2, 2, 1],
+                                         padding='SAME')
+
+        cl7_kernal = 2
+        cl7_in_depth = cl5_out_depth
+        cl7_out_depth = self.hps.deep_net_fkn * 4
+        cl7_layer_name = "layer7"
+        # layer7_conv:10
+        with tf.variable_scope(cl7_layer_name):
+            conv7_weight = tf.get_variable('weight',
+                                           [cl7_kernal, cl7_kernal, cl7_in_depth, cl7_out_depth],
+                                           initializer=tf.truncated_normal_initializer(stddev=0.1))
+            conv7_biases = tf.get_variable('bias', [cl7_out_depth])
+            conv7 = tf.nn.conv2d(layer6_pool, conv7_weight, strides=[1, 1, 1, 1], padding='VALID')
+            # layer7_conv = tf.nn.bias_add(conv7, conv7_biases)
+            layer7_conv = conv7
+            layer7_conv = tf.layers.batch_normalization(layer7_conv, axis=0, training=in_training)
+            layer7_conv = activateFunc(layer7_conv)
+
+        cl8_layer_name = 'layer8'
+        # layer8_pool:5
+        with tf.variable_scope(cl8_layer_name):
+            layer8_pool = tf.nn.max_pool(layer7_conv, [1, 2, 2, 1], strides=[1, 2, 2, 1],
+                                         padding='SAME')
+
+        cl9_kernal = 2
+        cl9_in_depth = cl7_out_depth
+        cl9_out_depth = self.hps.deep_net_fkn * 5
+        cl9_layer_name = "layer9"
+        # layer9_conv:4
+        with tf.variable_scope(cl9_layer_name):
+            conv9_weight = tf.get_variable('weight',
+                                           [cl9_kernal, cl9_kernal, cl9_in_depth, cl9_out_depth],
+                                           initializer=tf.truncated_normal_initializer(stddev=0.1))
+            conv9_biases = tf.get_variable('bias', [cl9_out_depth])
+            conv9 = tf.nn.conv2d(layer8_pool, conv9_weight, strides=[1, 1, 1, 1], padding='VALID')
+            layer9_conv = tf.nn.bias_add(conv9, conv9_biases)
+            # layer9_conv = tf.layers.batch_normalization(layer9_conv, axis=0, training=in_training)
+            layer9_conv = activateFunc(layer9_conv)
+
+        cl10_layer_name = 'layer10'
+        # layer10_pool:2
+        with tf.variable_scope(cl10_layer_name):
+            layer10_pool = tf.nn.max_pool(layer9_conv, [1, 2, 2, 1], strides=[1, 2, 2, 1],
+                                         padding='SAME')
+
+        cl11_kernal = 2
+        cl11_in_depth = cl9_out_depth
+        cl11_out_depth = self.hps.deep_net_fkn * 6
+        cl11_layer_name = "layer11"
+        # layer11_conv:1
+        with tf.variable_scope(cl11_layer_name):
+            conv11_weight = tf.get_variable('weight',
+                                           [cl11_kernal, cl11_kernal, cl11_in_depth, cl11_out_depth],
+                                           initializer=tf.truncated_normal_initializer(stddev=0.1))
+            conv11_biases = tf.get_variable('bias', [cl11_out_depth])
+            conv11 = tf.nn.conv2d(layer10_pool, conv11_weight, strides=[1, 1, 1, 1], padding='VALID')
+            layer11_conv = tf.nn.bias_add(conv11, conv11_biases)
+            # layer11_conv = tf.layers.batch_normalization(layer11_conv, axis=0, training=in_training)
+            layer11_conv = activateFunc(layer11_conv)
+
+        print("final outputs shape:", layer11_conv.get_shape().as_list())
+        return layer11_conv
+
+
+
+
+    def create_graph(self):
+        # create graph start
+        self.is_training_ph = tf.placeholder(tf.bool)
+
+        self.step = tf.Variable(self.init_step, trainable=False)
+
+
+        resnet = self.create_resnet(self._images, self.is_training_ph, activateFunc=self.activateFunction)
+        fcl1_inputs, fcl1_in_features = conv2fc(resnet)
+        # set outputs features
+        outputs_features = self.hps.num_classes
+        outputs = tf.layers.dense(fcl1_inputs, outputs_features)
+
+        correct_prediction = tf.equal(tf.argmax(outputs, axis=1), tf.argmax(self.labes, axis=1))
+        train_accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar('train acc', train_accuracy)
+
+        self.predictions = tf.nn.softmax(logits=outputs)
+
+        self.loss = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(
+                logits=outputs, labels=self.labes))
+
+        tf.summary.scalar('loss', self.loss)
+        self.merged = tf.summary.merge_all()
+
+        if self.mode == "train":
+            self.learning_rate = tf.placeholder(tf.float32)
+            update_op = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+            with tf.control_dependencies(update_op):
+                self.train_op = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(loss=self.loss,global_step=self.step)
+
+        self.init = tf.global_variables_initializer()
