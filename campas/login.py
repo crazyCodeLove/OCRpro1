d4336c50be3df9e025ee51e55f75566152286add
crazyCodeLove <crazyCodeLove@aliyun.com>
2018-05-15 16:05:16 +0800

    Update login.py
    add url

--- campas/login.py
+++ campas/login.py
@@ -2,7 +2,7 @@ import urllib
 import requests
 
 def check(id, passwd):
-    url = ""
+    url = "http://10.10.9.9:8080/eportal/index.jsp?wlanuserip=10.88.53.122&wlanacname=M18014-WS&ssid=&nasip=202.120.127.125&snmpagentip=&mac=9cb6d01a38bd&t=wireless-v2-plain&url=http://www.msftconnecttest.com/redirect&apmac=&nasid=M18014-WS&vid=947&port=50&nasportid=AggregatePort%204.09470000:947-0"
     form_data = {'userId':id,
                  'password':passwd,
                  'passwordEncrypt':False}
